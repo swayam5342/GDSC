9a05aa3392b18cad4fde4968933b33c355e39e61
swayam2307 <swayamsb50@gmail.com>
2024-09-21 00:15:09 +0530

    trying sql

---
+++ main.py
@@ -0,0 +1,22 @@
+from sqlconnect import connect_to_mysql
+import os
+from dotenv import load_dotenv
+load_dotenv('.env')
+config={
+    'host':os.getenv('host'),
+    'user':os.getenv('user'),
+    'password':os.getenv('password'),
+    'database':os.getenv('database')
+}
+
+con = connect_to_mysql(config)
+
+def get_data():
+    name=input("Enter the name : ")
+    gender=input("Enter the gender : ")
+    phone_number=int(input('Enter the Number if not')) or 1
+    return name, gender , phone_number
+con.cursor.execute
+data = con.cursor.execute("SELECT * FROM user_responses")
+for i in data:
+    print(i)
